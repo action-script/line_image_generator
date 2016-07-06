b0d6e4ee8854a194b8080e9e165c16a5e308061d
action-script <mail.actionscript@gmail.com>
2016-07-06 14:48:26 +0200

    webpack library setup and bower manager

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 
+var libraryName = 'Lig';
+var outputFile = 'lig.js';
+
 gulp.task('default', function() {
    return gulp.src('js/main.js')
       .pipe(webpack({
          output: {
-            filename: "lig.js"
+            filename: outputFile,
+            library: libraryName,
+            libraryTarget: 'umd',
+            umdNamedDefine: true
          }
       }))
       .pipe(gulp.dest('build/'));
